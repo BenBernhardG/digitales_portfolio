0e2c3b39a3f133264ed28119d26da3703172dc32
BenBernhardG <138679020+BenBernhardG@users.noreply.github.com>
2023-11-15 21:22:59 +0100

    update script.js

--- WebRoot/script.js
+++ WebRoot/script.js
@@ -1,6 +1,8 @@
 // BenBernhardG (Ben Giese) 2023
+/*
 const asciiTable = require('ascii-tables');
 
 const projectTable = new asciiTable().setHeading('Project', 'Finished').setTitle('');
 
 projectTable.addRow('undifiend', 'yes')
+*/
